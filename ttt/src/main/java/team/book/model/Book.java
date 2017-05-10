18451efbd5cf09cfd568cf3bc2c8db75e8c4c8f8
65 <65@pc611>
2017-05-10 21:42:38 +0900

    asd

--- ttt/src/main/java/team/book/model/Book.java
+++ ttt/src/main/java/team/book/model/Book.java
@@ -1,5 +1,6 @@
 package team.book.model;
 
+import java.util.Date;
 import java.util.List;
 
 public class Book {
@@ -7,6 +8,9 @@ public class Book {
 	private int page;
 	private int tpg;
 	private List<BookVo> list;
+	private String rentaluser;
+	private Date returndate;
+	private BookVo vo;
 	
 	public int getPage() {
 		return page;
@@ -26,6 +30,26 @@ public class Book {
 	public void setList(List<BookVo> list) {
 		this.list = list;
 	}
+	public String getRentaluser() {
+		return rentaluser;
+	}
+	public void setRentaluser(String rentaluser) {
+		this.rentaluser = rentaluser;
+	}
+	public Date getReturndate() {
+		return returndate;
+	}
+	public void setReturndate(Date returndate) {
+		this.returndate = returndate;
+	}
+
+	public BookVo getVo() {
+		return vo;
+	}
+	public void setVo(BookVo vo) {
+		this.vo = vo;
+	}
+
 	
 	
 }
